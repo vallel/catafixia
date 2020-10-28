8c962c2fc52566a33a010d4e22bbd2efae8a74a0
Vallel Blanco <vallelblanco@gmail.com>
2020-10-27 22:37:46 -0700

    Init

---
+++ js/app.js
@@ -0,0 +1,65 @@
+var app = new Vue({
+    el: '#main',
+    data: {
+        totalSurprises: 70,
+        prices: [],
+        showAddPrice: false,
+        newPrice: null,
+        showSettings: false
+    },
+    created: function() {
+        if (localStorage.prices) {
+            this.prices = JSON.parse(localStorage.prices);
+        }
+    },
+    methods: {
+        showPrice: function(event) {
+            var button = event.target;
+
+            if (button.classList.contains('price-btn')) {
+                button.classList.remove('price-btn');
+
+                var value = 0;
+                if (this.prices.length) {
+                    var rand = Math.random();
+                    value = Math.round(rand * (this.prices.length - 1) + 1);
+                }
+
+                button.textContent = value;
+            }
+            return false;
+        },
+        onEnterNewPrice: function(event) {
+            if (event.keyCode === 13) {
+                event.preventDefault();
+                this.addPrice();
+            } else if (event.keyCode == 27) {
+                event.preventDefault();
+                this.newPrice = "";
+                this.showAddPrice = false;
+            }
+        },
+        addPrice: function() {
+            if (this.newPrice) {
+                this.prices.push(this.newPrice);
+                localStorage.prices = JSON.stringify(this.prices);
+                this.newPrice = "";
+                this.$refs.newPrice.focus();
+            }
+        },
+        deletePrice: function(priceIndex) {
+            this.prices.splice(priceIndex, 1);
+            localStorage.prices = JSON.stringify(this.prices);
+        },
+        showAddPriceForm: function(show, event) {
+            event.preventDefault();
+            this.showAddPrice = show;
+        },
+        onShowSettings: function(show) {
+            // this.showSettings = show;
+        },
+        onSaveSettings: function() {
+            this.onShowSettings(false);
+        }
+    }
+});
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Catafixia
+
+[Settings icon](https://freeicons.io/free-setting-and-configuration-icons/gear-settings-setting-wheel-icon-9576) made by [Raj Dev](https://freeicons.io/profile/714) from www.freeicons.io
